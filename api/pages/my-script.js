c98e77f513f335e5b3f63bc4853fd472d8f2a5cb
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-03-04 03:15:46 +0300

    Delete api directory

--- api/pages/my-script.js
+++
@@ -1,5 +0,0 @@
-import '../../api/script.js'; // Import and execute the script file
-
-export default function handler(req, res) {
-  res.status(200).json({ message: 'Script executed successfully' });
-}
